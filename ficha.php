918cf61a7be0872aeceb822079784b03fc8517c2
AntonioGodoyCarneiro <antoniogodoycarneiro@gmail.com>
2025-02-21 12:02:58 -0300

    finalizamos

--- ficha.php
+++ ficha.php
@@ -1,6 +1,6 @@
 <?php
 
-//Para puxar informação da URL, que nesse caso é oid_aluno
+//Para puxar informação da URL, que nesse caso é o id_aluno
 $id_aluno = $_GET['id_aluno'];
 
 
@@ -10,7 +10,7 @@ $password = '';
 
 $banco = new PDO($dsn, $user, $password);
 
-$select = "SELECT * FROM tb_info_alunos where id_alunos = " . $id_aluno;
+$select = "SELECT tb_info_alunos.*, tb_aluno.nome FROM tb_info_alunos INNER JOIN tb_aluno ON tb_aluno.id = tb_info_alunos.id_alunos WHERE tb_info_alunos.id_alunos= " . $id_aluno;
 
 $dados = $banco->query($select)->fetch();
 
@@ -19,57 +19,52 @@ $dados = $banco->query($select)->fetch();
 
 
 ?>
-
-
-
-
-
-
-
-
-
-
-
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
- 
+
+
 <style>
     main {
         display: flex;
         flex-direction: column;
         align-items: center;
     }
- 
+
     form {
         width: 600px;
     }
+
+    img {
+        width: 200px;
+        object-fit: cover;
+    }
 </style>
- 
+
 <main class="container text-center my-5">
- 
-    <img src="./img/Kratos_GoW_Ragnarok.jpg" alt="imagem do perfil" class="img-thumbnail">
- 
+
+    <img src="./img/<?= $dados['img'] ?>" alt="imagem do perfil" class="img-thumbnail">
+
     <form action="#">
- 
+
         <label for="nome">Nome:</label class="form-control">
-        <input type="text" value="Antônio Gabriel " disabled class="form-control">
+        <input type="text" value="<?= $dados['nome'] ?>" disabled class="form-control">
         <div class="row mt-2 ">
- 
+
             <div class="col">
                 <label for="telefone">Telefone:</label>
-                <input type="number" value="<?=$dados['telefone']?>" disabled class="form-control">
+                <input type="number" value="<?= $dados['telefone'] ?>" disabled class="form-control">
             </div>
- 
+
             <div class="col">
                 <label for="email">Email:</label>
-                <input type="email" value="<?=$dados['email']?>" disabled class="form-control">
+                <input type="email" value="<?= $dados['email'] ?>" disabled class="form-control">
             </div>
         </div>
         <div class="row mt-2">
             <div class="col">
                 <label for="data_nascimento">Data de Nascimento:</label>
-                <input type="date" value="<?=$dados["nascimento"]?>" disabled class="form-control">
+                <input type="date" value="<?= $dados["nascimento"] ?>" disabled class="form-control">
             </div>
- 
+
             <div class="col my-4 pt-2">
                 <input type="checkbox" class="form-check-input">
                 <label for="frequente">Frequente:</label>
